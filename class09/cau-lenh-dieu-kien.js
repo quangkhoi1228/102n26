143d869fd5ee2f914d26c089517b1b493a9b1587
khoitq-acl <khoitq@americancodelab.com>
2024-04-16 22:21:44 +0700

    Add class 09

---
+++ class09/cau-lenh-dieu-kien.js
@@ -0,0 +1,63 @@
+/* 
+
+
+# if
+
+if($bieu-thuc){
+  // block code sẽ chạy khi $bieu-thuc đúng
+}
+
+
+
+
+*/
+
+// var age = 15;
+// if (age >= 18) {
+//   console.log("Bạn đã trưởng thành.");
+// }
+
+/* 
+
+# else if
+
+
+if($bieu-thuc1){
+  // block code sẽ chạy khi $bieu-thuc1 đúng
+} 
+else if($bieu-thuc2){
+  // block code sẽ chạy khi $bieu-thuc2 đúng
+} 
+
+*/
+
+var age = 1
+
+if (age >= 18) {
+  console.log("Bạn là sinh viên")
+}
+else if (age >= 15 && age < 18) {
+  console.log("Bạn là học sinh trung học phổ thông")
+}
+else {
+  console.log("Bạn là học sinh THCS hoặc tiểu học")
+}
+
+
+
+
+
+/*
+
+1. Số chẵn chia hết cho 2
+
+2. Năm nhuận là năm chia hết cho 4
+
+3. Điểm số 
+  >=8: Giỏi
+  6.5<= x <8: Khá
+  5<= x <6.5: Trung bình
+  <5: Yếu
+
+
+*/
---
+++ class09/toan-tu-va-bieu-thuc.js
@@ -0,0 +1,14 @@
+var sum = 10 + 5; // sum = 15
+console.log("Sum: ", sum)
+
+var difference = 10 - 5; // difference = 5
+console.log("difference: ", difference)
+
+var product = 10 * 5; // product = 50
+console.log("product: ", product)
+
+var quotient = 10 / 5; // quotient = 2
+console.log("quotient: ", quotient)
+
+var remainder = 10 % 3; // remainder = 1
+console.log("remainder: ", remainder)
